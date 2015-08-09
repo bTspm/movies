8cc7630c61edf7149c0da76c73d58aca350d1038
btspm <btspm10@gmail.com>
2015-08-09 04:22:56 -0500

    dev.rb edited for heroku

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,7 +40,7 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
 
   # For devise gem
-  #config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 config.assets.initialize_on_precompile = false
 
 end
